e26362a92f0da93db45d8bf4428847a45073dc2a
MakHack <MakHack@pc-mak>
2015-01-27 23:14:26 +0100

    requete

--- instal/select.sql
+++ instal/select.sql
@@ -1,5 +1,3 @@
-
-
 select  u.user_id, 
         u.login,
         u.status,
@@ -15,4 +13,27 @@ INNER JOIN task t
 INNER JOIN status s
     on t.status_id = s.status_id
 WHERE u.status != 'manager'
+
+SELECT *
+FROM user
+WHERE user.password = 'user1'
+AND user.login = 'user1'
+
+SELECT  t.task_id,
+        t.title,
+        t.description, 
+        t.deadline, 
+        t.estimated_time,
+        s.description,
+        u.user_id
+FROM assignment a
+INNER JOIN task t
+    on a.task_id = t.task_id
+INNER JOIN status s
+    on t.status_id = s.status_id
+INNER JOIN user u
+    on a.user_id = u.user_id
+WHERE u.user_id = 3
+
+
 
